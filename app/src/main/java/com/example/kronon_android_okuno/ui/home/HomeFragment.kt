6d36ebc3734452533678cc987acb8b166683f2a4
h-okuno <hirokki.oku@gmail.com>
2020-11-20 14:29:16 +0900

    アカウント画面、完成

--- app/src/main/java/com/example/kronon_android_okuno/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/kronon_android_okuno/ui/home/HomeFragment.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.CalendarView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -23,8 +21,8 @@ class HomeFragment : Fragment() {
     ): View? {
         homeViewModel =
                 ViewModelProvider(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
+        val root = inflater.inflate(R.layout.fragment_calendar, container, false)
+        val textView: TextView = root.findViewById(R.id.text_calendar)
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
