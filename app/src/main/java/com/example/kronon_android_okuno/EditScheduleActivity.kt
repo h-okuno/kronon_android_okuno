ddf0366bf46b01dbc95fcc3f3053483a57d128d6
h-okuno <hirokki.oku@gmail.com>
2020-12-14 15:45:18 +0900

    activity命名修正

--- app/src/main/java/com/example/kronon_android_okuno/EditScheduleActivity.kt
+++ app/src/main/java/com/example/kronon_android_okuno/EditScheduleActivity.kt
@@ -3,7 +3,7 @@ package com.example.kronon_android_okuno
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class EditScheduleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.edit_schedule)
