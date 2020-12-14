b7c2873a0e1c07473c004298f928e8eb89aa96c6
h-okuno <hirokki.oku@gmail.com>
2020-12-14 15:36:01 +0900

    activity命名修正

--- app/src/main/java/com/example/kronon_android_okuno/InputScheduleActivity.kt
+++ app/src/main/java/com/example/kronon_android_okuno/InputScheduleActivity.kt
@@ -3,7 +3,7 @@ package com.example.kronon_android_okuno
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class InputScheduleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.input_schedule)
