b3c612c9197d086a2b55178e2fe72033f980bba4
h-okuno <hirokki.oku@gmail.com>
2020-12-14 19:34:11 +0900

    logout画面まで結合

--- app/src/main/java/com/example/kronon_android_okuno/LoginActivity.kt
+++ app/src/main/java/com/example/kronon_android_okuno/LoginActivity.kt
@@ -1,13 +1,20 @@
 package com.example.kronon_android_okuno
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 
 class LoginActivity: AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?){
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
-    }
 
+        val button_login = findViewById<Button>(R.id.button_login)
+        button_login.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
 }
--- app/src/main/java/com/example/kronon_android_okuno/TopViewActivity.kt
+++ app/src/main/java/com/example/kronon_android_okuno/TopViewActivity.kt
@@ -1,11 +1,22 @@
 package com.example.kronon_android_okuno
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
 
 class TopViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_top_view)
+
+        val top_view_button = findViewById<Button>(R.id.top_view_button)
+        top_view_button.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
