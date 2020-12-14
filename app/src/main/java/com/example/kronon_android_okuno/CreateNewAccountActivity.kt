1da04c941ea37e1e114093eb64f5c5c28624b4ee
h-okuno <hirokki.oku@gmail.com>
2020-12-14 15:23:43 +0900

    activity命名修正

---
+++ app/src/main/java/com/example/kronon_android_okuno/CreateNewAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.example.kronon_android_okuno
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateNewAccountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_newaccount)
+
+    }
+}
