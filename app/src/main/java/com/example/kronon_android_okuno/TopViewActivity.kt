eff19f5787cc5a36da6aaed15fd8cec82a920b89
h-okuno <hirokki.oku@gmail.com>
2020-12-14 14:55:11 +0900

    命名変更

--- app/src/main/java/com/example/kronon_android_okuno/TopViewActivity.kt
+++ app/src/main/java/com/example/kronon_android_okuno/TopViewActivity.kt
@@ -3,9 +3,9 @@ package com.example.kronon_android_okuno
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class TopViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_top_view)
     }
 }
